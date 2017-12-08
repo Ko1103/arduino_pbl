9036d34234050ceeecafb20cdec17644e86a51ba
KO-YAMAKO <ko.foot1103@gmail.com>
2017-12-08 18:01:38 +0900

    edited climb

--- sketch_oct19b/sketch_oct19b.ino
+++ sketch_oct19b/sketch_oct19b.ino
@@ -48,16 +48,16 @@ void loop() {
   moveServo(fore_right.ankle, 90, 90); //right ankle 150度でまっすぐになる
   moveServo(fore_right.knee, 120,120); // right knee の初期値として足を下につかせる
   moveServo(fore_right.joint, 100,100); //right jointの初期値として正面を向かせる
-  moveServo(back_right.knee, 90,90);
+  moveServo(back_right.knee, 80,80);
   moveServo(back_right.joint, 110,110);
   moveServo(back_left.knee, 110,110);
   moveServo(back_left.joint, 100,100);
 //  walk();
-  climb();
+//  climb();
 }
 
 void walk(){
-  moveServo(fore_left.knee, 100,60);
+  moveServo(fore_left.knee, 120,60);
   moveServo(fore_left.knee, 60,120);
   moveServo(fore_right.knee, 100,60);
   moveServo(fore_right.knee, 60, 120);
@@ -75,27 +75,29 @@ void walk(){
 }
 
 void climb(){
-  moveServo(fore_left.ankle, 100, 150);
+  moveServo(fore_left.ankle, 100, 170);
   moveServo(fore_left.knee, 100,30);
-  moveServo(fore_left.ankle, 150, 100);
+  moveServo(fore_left.ankle, 170, 100);
   moveServo(fore_left.knee, 30,100);
 
-  moveServo(fore_right.ankle, 100, 150);
+  moveServo(fore_right.ankle, 100, 170);
   moveServo(fore_right.knee, 100,30);
-  moveServo(fore_right.ankle, 150, 100);
+  moveServo(fore_right.ankle, 170, 100);
   moveServo(fore_right.knee, 30, 100);
 
-  moveServo(back_left.joint, 100, 80);
-  moveServo(back_left.knee, 110, 50);
-  moveServo(back_left.joint, 100, 140);
-  moveServo(back_left.knee, 50, 110);
+  moveServo(back_left.joint, 100, 20);
+  moveServo(back_left.knee, 110, 30);
+  moveServo(back_left.joint, 20, 140);
+  moveServo(back_left.knee, 30, 110);
   moveServo(back_left.joint, 140,100);
+  
   moveServo(fore_right.knee, 100,120);
   moveServo(fore_left.knee, 100, 120);
-  moveServo(back_right.joint, 110, 130);
-  moveServo(back_right.knee, 90, 140);
-  moveServo(back_right.joint, 130, 60);
-  moveServo(back_right.knee, 140, 90);
+  
+  moveServo(back_right.joint, 110, 170);
+  moveServo(back_right.knee, 90, 150);
+  moveServo(back_right.joint, 170, 60);
+  moveServo(back_right.knee, 150, 90);
   moveServo(back_right.joint, 60,110);
 }
 
